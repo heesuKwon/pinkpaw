b10034ebed6150403c9b128addf768e58cdb6217
68652667 <68652667@hanmail.net>
2019-08-12 17:56:04 +0900

    후기게시판 검색기능

--- PinkPaw/src/com/pinkpaw/board/reviewboard/model/dao/ReviewDAO.java
+++ PinkPaw/src/com/pinkpaw/board/reviewboard/model/dao/ReviewDAO.java
@@ -452,10 +452,9 @@ public class ReviewDAO {
 			int end = cPage*numPerPage;
 			
 			pstmt.setString(1, kind);
-			pstmt.setString(2, key);
-			pstmt.setString(3, "%"+keyword+"%");
-			pstmt.setInt(4, start);
-			pstmt.setInt(5, end);
+			pstmt.setString(2, "%"+keyword+"%");
+			pstmt.setInt(3, start);
+			pstmt.setInt(4, end);
 			
 			rset = pstmt.executeQuery();
 			
